3f50566de3c3bbed6b734f0d0f9e01f35710805a
Raphael Hanneken <raphaelhanneken@me.com>
2015-12-11 00:24:50 +0100

    Made colors changeable from the UI

--- Example/Example/MainWindowController.swift
+++ Example/Example/MainWindowController.swift
@@ -23,5 +23,10 @@ class MainWindowController: NSWindowController {
 
       // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
   }
+
+  @IBAction func changeColors(sender: NSButton) {
+    self.text.gutterForegroundColor = NSColor(calibratedHue: 0, saturation: 0, brightness: 0, alpha: 1)
+    self.text.gutterBackgroundColor = NSColor(calibratedHue: 0, saturation: 0, brightness: 0.9, alpha: 1)
+  }
     
 }

697033497fd6e645bdc612013c320873bc851491
Raphael Hanneken <raphaelhanneken@me.com>
2015-12-10 22:41:21 +0100

    Added Example Project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Raphael Hanneken on 10/12/15.
+//  Copyright © 2015 Raphael Hanneken. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+

e3d0b469972aa2b79cde95aa43b29bfc9bbf75a4
Raphael Hanneken <raphaelhanneken@me.com>
2015-12-10 22:23:46 +0100

    Implemented drawHashMarksAndLabelsInRect

--- LineNumberTextView/LineNumberTextView/LineNumberGutter.swift
+++ LineNumberTextView/LineNumberTextView/LineNumberGutter.swift
@@ -51,4 +51,95 @@ class LineNumberGutter: NSRulerView {
   required init?(coder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
   }
+
+  ///  Draws the line numbers.
+  ///
+  ///  - parameter rect: NSRect to draw the gutter view in.
+  override func drawHashMarksAndLabelsInRect(rect: NSRect) {
+    // Unwrap the clientView, the layoutManager and the textContainer, since we'll
+    // them sooner or later.
+    guard let textView      = self.clientView as? NSTextView,
+              layoutManager = textView.layoutManager,
+              textContainer = textView.textContainer,
+              content       = textView.string else {
+        return
+    }
+
+    // Get the range of the currently visible glyphs.
+    let visibleGlyphsRange = layoutManager.glyphRangeForBoundingRect(textView.visibleRect, inTextContainer: textContainer)
+
+    // Check how many lines are out of the current bounding rect.
+    var lineNumber: Int = 1
+    do {
+      // Define a regular expression to find line breaks.
+      let newlineRegex = try NSRegularExpression(pattern: "\n", options: [])
+      // Check how many lines are out of view; From the glyph at index 0
+      // to the first glyph in the visible rect.
+      lineNumber += newlineRegex.numberOfMatchesInString(content, options: [], range: NSMakeRange(0, visibleGlyphsRange.location))
+    } catch {
+      return
+    }
+
+    // Get the index of the first glyph in the visible rect, as starting point...
+    var firstGlyphOfLineIndex = visibleGlyphsRange.location
+
+    // ...then loop through all visible glyphs, line by line.
+    while firstGlyphOfLineIndex < NSMaxRange(visibleGlyphsRange) {
+      // Get the character range of the line we're currently in.
+      let charRangeOfLine  = (content as NSString).lineRangeForRange(NSRange(location: layoutManager.characterIndexForGlyphAtIndex(firstGlyphOfLineIndex), length: 0))
+      // Get the glyph range of the line we're currently in.
+      let glyphRangeOfLine = layoutManager.glyphRangeForCharacterRange(charRangeOfLine, actualCharacterRange: nil)
+
+      var firstGlyphOfRowIndex = firstGlyphOfLineIndex
+      var lineWrapCount        = 0
+
+      // Loop through all rows (soft wraps) of the current line.
+      while firstGlyphOfRowIndex < NSMaxRange(glyphRangeOfLine) {
+        // The effective range of glyphs within the current line.
+        var effectiveRange = NSRange(location: 0, length: 0)
+        // Get the rect for the current line fragment.
+        let lineRect = layoutManager.lineFragmentRectForGlyphAtIndex(firstGlyphOfRowIndex, effectiveRange: &effectiveRange, withoutAdditionalLayout: true)
+
+        // Draw the current line number;
+        // When lineWrapCount > 0 the current line spans multiple rows.
+        if lineWrapCount == 0 {
+          self.drawLineNumber(lineNumber, atYPosition: lineRect.minY)
+        } else {
+          break
+        }
+
+        // Move to the next row.
+        firstGlyphOfRowIndex = NSMaxRange(effectiveRange)
+        lineWrapCount++
+      }
+
+      // Move to the next line.
+      firstGlyphOfLineIndex = NSMaxRange(glyphRangeOfLine)
+      lineNumber++
+    }
+
+    // Draw another line number for the extra line fragment.
+    if let _ = layoutManager.extraLineFragmentTextContainer {
+      self.drawLineNumber(lineNumber, atYPosition: layoutManager.extraLineFragmentRect.minY)
+    }
+  }
+
+
+  func drawLineNumber(num: Int, atYPosition yPos: CGFloat) {
+    // Unwrap the text view.
+    guard let textView = self.clientView as? NSTextView,
+              font     = textView.font else {
+      return
+    }
+    // Define attributes for the attributed string.
+    let attrs = [NSFontAttributeName: font]
+    // Define the attributed string.
+    let attributedString = NSAttributedString(string: "\(num)", attributes: attrs)
+    // Get the NSZeroPoint from the text view.
+    let relativePoint    = self.convertPoint(NSZeroPoint, fromView: textView)
+    // Calculate the x position, within the gutter.
+    let xPosition        = GUTTER_WIDTH - (attributedString.size().width + 5)
+    // Draw the attributed string to the calculated point.
+    attributedString.drawAtPoint(NSPoint(x: xPosition, y: relativePoint.y + yPos))
+  }
 }

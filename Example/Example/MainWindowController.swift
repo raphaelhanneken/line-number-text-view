f7a011ef63b6bcd729384f73a97ab94441016f59
Raphael Hanneken <raphaelhanneken@me.com>
2015-12-10 22:42:45 +0100

    Added MainWindowController

---
+++ Example/Example/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  Example
+//
+//  Created by Raphael Hanneken on 10/12/15.
+//  Copyright © 2015 Raphael Hanneken. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
